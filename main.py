464392bad154284c1cc6517d100f1a2d81e1a4a9
W1nnie69 <danizye@gmail.com>
2024-08-23 12:54:20 +0800

    inital commit
    this is commands, gonna be using interactions now

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import os
+import asyncio
+
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+with open("token.txt") as file:
+    token = file.read()
+    print(token)
+
+
+async def load():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+
+async def main():
+    async with bot:
+        await load()
+        await bot.start(token)
+
+
+asyncio.run(main())
