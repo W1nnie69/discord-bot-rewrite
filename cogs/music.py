9d4e4cf3314ff081b1c3400410e196359e971a39
W1nnie69 <danizye@gmail.com>
2024-08-23 14:59:22 +0800

    ver 1.0
    Basic cog is working!!!!!!!!!

--- cogs/music.py
+++ cogs/music.py
@@ -1,12 +1,30 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 
 class Music(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("music cog loaded")
 
+
+    @commands.command()
+    async def sync(self, ctx) -> None:
+        fmt = await ctx.bot.tree.sync(guild=ctx.guild)
+        await ctx.send(f"Synced {len(fmt)} commands.")
+        
+
+    @app_commands.command(name = "ping", description = "testing")
+    async def ping(self, interaction: discord.Interaction):
+        await interaction.response.send_message("pong")
+
+    # async def cog_load(self):
+    #     guild_id = 1259846523626197043  # Replace with your server's ID or set to None for global
+    #     guild = discord.Object(id=guild_id) if guild_id else None
+    #     await self.bot.tree.sync(guild=guild)
 
 async def setup(bot):
-    await bot.add_cog(Music(bot))
+    await bot.add_cog(Music(bot), guilds=[discord.Object(id=1259846523626197043)])
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 import os
 import asyncio
+import config
 
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), application_id = 887197279386214480)
 
-with open("token.txt") as file:
-    token = file.read()
-    print(token)
+@bot.event
+async def on_ready():
+    print("online")
+
 
 
 async def load():
@@ -16,10 +19,23 @@ async def load():
         if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
 
+
 async def main():
-    async with bot:
-        await load()
-        await bot.start(token)
+    await load()
+    await bot.start(config.token)
+
+
+
+# Command to sync slash commands (optionally for a specific guild)
+# @bot.command()
+# async def sync(ctx, guild_id: int = None):
+#     if guild_id:
+#         guild = discord.Object(id=guild_id)
+#         synced = await bot.tree.sync(guild=guild)
+#         await ctx.send(f"Synced {len(synced)} command(s) to guild {guild_id}.")
+#     else:
+#         synced = await bot.tree.sync()
+#         await ctx.send(f"Synced {len(synced)} command(s) globally.")
 
 
 asyncio.run(main())
